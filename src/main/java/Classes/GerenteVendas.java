a28a23fad7901b844f63c0215d2849e7e2d67264
anderson.jscosta <anderson.jscosta@CAS567072W1028.senacsp.edu.br>
2018-11-29 20:30:19 -0200

    PII JORGE

---
+++ src/main/java/Classes/GerenteVendas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author AnD James
+ */
+//Gerente de Vendas
+public class GerenteVendas extends Funcionarios{
+    
+    
+    
+      public GerenteVendas(String nome, String rg, String cpf, String filial, String cargo) {
+  
+        
+        super(  nome,  rg, cpf,  filial, cargo );
+  
+    }
+      
+      
+      
+      
+      
+      
+      
+}
